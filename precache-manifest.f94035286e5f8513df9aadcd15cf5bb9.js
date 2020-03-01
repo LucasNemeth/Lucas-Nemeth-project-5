6dbac80727706408b6e6921a4c21f9f30c590879
LucasNemeth <l_orion_n@hotmail.com>
2020-03-01 14:10:05 -0500

    Updates

---
+++ precache-manifest.f94035286e5f8513df9aadcd15cf5bb9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28826edaa600a6182bb669978077a82e",
+    "url": "/Lucas-Nemeth-project-5/index.html"
+  },
+  {
+    "revision": "acdad96e711fb7132a17",
+    "url": "/Lucas-Nemeth-project-5/static/css/2.086da048.chunk.css"
+  },
+  {
+    "revision": "abdd09f37edc38e33086",
+    "url": "/Lucas-Nemeth-project-5/static/css/main.94db63ee.chunk.css"
+  },
+  {
+    "revision": "acdad96e711fb7132a17",
+    "url": "/Lucas-Nemeth-project-5/static/js/2.8f12a658.chunk.js"
+  },
+  {
+    "revision": "80f88c8a2fc44f425b1cf753399d693e",
+    "url": "/Lucas-Nemeth-project-5/static/js/2.8f12a658.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abdd09f37edc38e33086",
+    "url": "/Lucas-Nemeth-project-5/static/js/main.0acc8349.chunk.js"
+  },
+  {
+    "revision": "420388ddf26467d81ffe",
+    "url": "/Lucas-Nemeth-project-5/static/js/runtime-main.b0bce112.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Lucas-Nemeth-project-5/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Lucas-Nemeth-project-5/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Lucas-Nemeth-project-5/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Lucas-Nemeth-project-5/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Lucas-Nemeth-project-5/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
